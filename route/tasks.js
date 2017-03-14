8ba70013d040ad1d21ae272ae6aff89c5cbd8644
Ranjan Kumar Pandey <ranjan@smartbox.in>
2017-03-14 15:11:56 +0530

    started working for database lavel things

--- route/tasks.js
+++ route/tasks.js
@@ -1,8 +1,16 @@
 var express=require('express');
 var router=express.Router();
-
+var mongojs=require('mongojs');
+var db =mongojs('mongodb://localhost:27017/smartbox',['users']);
 router.get('/tasks',function(req,res,next){
-	res.send("hi this is index page");
+	db.users.find(function(error,task){
+		if(error){
+			res.send(error);
+		}else{
+			res.json(task);
+		}
+	})
+	
 })
 
 module.exports=router;
